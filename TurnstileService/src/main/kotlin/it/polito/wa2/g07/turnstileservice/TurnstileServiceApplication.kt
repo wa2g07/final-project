5c083f5699ff1744cba09a677389bea247f16335
ALESSANDRO IANDOLI s290673 <s290673@studenti.polito.it>
2022-07-20 16:06:32 +0200

    TurnstileService: full refactor

---
+++ TurnstileService/src/main/kotlin/it/polito/wa2/g07/turnstileservice/TurnstileServiceApplication.kt
@@ -0,0 +1,54 @@
+package it.polito.wa2.g07.turnstileservice
+
+import it.polito.wa2.g07.turnstileservice.kafka.TransitInfo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.support.MessageBuilder
+import java.lang.Thread.sleep
+import java.time.LocalDateTime
+import java.util.*
+
+@SpringBootApplication
+class TurnstileServiceApplication {
+
+    @Autowired
+    lateinit var kafkaTemplate: KafkaTemplate<String,Any?>
+
+    @Bean
+    fun runner() = ApplicationRunner{
+        val message1: org.springframework.messaging.Message<TransitInfo> = MessageBuilder
+                .withPayload(TransitInfo(
+                        turnstileId = 1,
+                        ticketId = 123455909,
+                        ticketType = "type3",
+                        timestamp = Date(),
+                        username = "customer"
+
+                )).setHeader(KafkaHeaders.TOPIC, "transit")
+                .build()
+        val message2: org.springframework.messaging.Message<TransitInfo> = MessageBuilder
+                .withPayload(TransitInfo(
+                        turnstileId = 1,
+                        ticketId = 123455907,
+                        ticketType = "type1",
+                        timestamp = Date(),
+                        username = "customer"
+
+                )).setHeader(KafkaHeaders.TOPIC, "transit")
+                .build()
+        while(true){
+            kafkaTemplate.send(message1)
+            sleep(3000)
+            kafkaTemplate.send(message2)
+            sleep(3000)
+        }
+    }
+}
+fun main(args: Array<String>) {
+    runApplication<TurnstileServiceApplication>(*args)
+}
---
+++ TurnstileService/src/main/kotlin/it/polito/wa2/g07/turnstileservice/kafka/TransitInfoSerializer.kt
@@ -0,0 +1,21 @@
+package it.polito.wa2.g07.turnstileservice.kafka
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.kafka.common.errors.SerializationException
+import org.apache.kafka.common.serialization.Serializer
+import org.slf4j.LoggerFactory
+
+
+class TransitInfoSerializer : Serializer<TransitInfo> {
+  private val objectMapper = ObjectMapper()
+  private val log = LoggerFactory.getLogger(javaClass)
+
+  override fun serialize(topic: String?, data: TransitInfo?): ByteArray? {
+    log.info("Serializing...")
+    return objectMapper.writeValueAsBytes(
+      data ?: throw SerializationException("Error when serializing BillingInformation to ByteArray[]")
+    )
+  }
+
+  override fun close() {}
+}
