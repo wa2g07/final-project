0b1e354140cc5b995d1a905e1addd52d9c7fd6c3
ALESSANDRO IANDOLI s290673 <s290673@studenti.polito.it>
2022-07-20 09:29:21 +0200

    TurnstileServiceTest: added tunrstile service test

---
+++ TurnstileServiceTest/src/test/kotlin/it/polito/wa2/g07/turnstileservicetest/TurnstileServiceTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2.g07.turnstileservicetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TurnstileServiceTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
