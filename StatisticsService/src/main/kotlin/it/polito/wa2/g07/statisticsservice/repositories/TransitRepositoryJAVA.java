09db2976e73e06807000ada98d8452b675ad4baa
alessio <alessiocorvaglia98@gmail.com>
2022-07-22 11:47:18 +0200

    add some comments

--- StatisticsService/src/main/kotlin/it/polito/wa2/g07/statisticsservice/repositories/TransitRepositoryJAVA.java
+++ StatisticsService/src/main/kotlin/it/polito/wa2/g07/statisticsservice/repositories/TransitRepositoryJAVA.java
@@ -38,4 +38,44 @@ public interface TransitRepositoryJAVA extends ReactiveMongoRepository<Transit, 
     })
     Flux<TransitCountDTO> getMyTransitsCountPerHour(Date from, Date to, String username);
 }
+
+
+
+JWT SOLO CUSTOMER
+eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJjdXN0b21lciIsImlhdCI6MTY1MTI0MzQ1MiwiZXhwIjoxNjU5OTk5OTk5LCJyb2xlcyI6IkNVU1RPTUVSIn0.7qdzAOQo7NqeG53bd187cXztig3sVafYd_1iXoOKmOFZtjjAmwVIlu0bemtRsuBN
+
+JWT SOLO ADMIN
+eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhZG1pbi1uaWNrIiwiaWF0IjoxNjUxMjQzNDUyLCJleHAiOjE2NTk5OTk5OTksInJvbGVzIjoiQURNSU4ifQ.Y927iZAlQIpADuvCI11gy0mq1z6qq07AdcS5YcmJRKdpuNPkWkuocc_GpyPbBLbt
+
+JWT ENTRAMBI
+eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhZG1pbi1uaWNrIiwiaWF0IjoxNjUxMjQzNDUyLCJleHAiOjE2NTk5OTk5OTksInJvbGVzIjoiQ1VTVE9NRVJ8QURNSU4ifQ.K84pqtp9YuP22PkEUqAk1Fxp8JHSagQWVSEPqka9I6xMwql5OjV-DwiAYpfa6cFP
+
+JWT EMBEDDED SYSTEM:
+eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJjdXN0b21lciIsImlhdCI6MTY1MTI0MzQ1MiwiZXhwIjoxNjU5OTk5OTk5LCJyb2xlcyI6IkVNQkVEREVEU1lTVEVNIn0.kNN0WMynsJq9Sal_rBIW5rXCA6r1sxGOVGjc0Q3puphiCHPUh3diEOk0EHpTagfI
+
+
+db.transits.aggregate([
+        {$addFields: {date: {$dateToString: {format: "%Y%m%d", date: "$timestamp"}}}},
+        {$match: {date: {$gte: "20220701", $lte: "20220722"}}},
+        {$project: {date: 1}},
+        {$group: {_id: "$date", count: {$count: {}}}}
+])
+
+db.transits.aggregate([
+    {$addFields: {date: {$dateToString: {format: "%Y%m%d", date: "$timestamp"}}}},
+    {$match: {date: "20220722"}},
+    {$addFields: {hour: {$hour: "$timestamp"}}},
+    {$project: {hour: 1}},
+    {$group: {_id: "$hour", count: {$count: {}}}}
+])
+
+
+db.transits.aggregate([
+    {$match: {username: "customer2"}},
+    {$addFields: {date: {$dateToString: {format: "%Y%m%d", date: "$timestamp"}}}},
+    {$match: {date: {$gte: "20220701", $lte: "20220722"}}},
+    {$addFields: {hour: {$hour: "$timestamp"}}},
+    {$project: {hour: 1}},
+    {$group: {_id: "$hour", count: {$count: {}}}}
+])
 */
